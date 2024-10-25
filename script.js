240e876b3a2461887a66c2da9deb35ceb8b49f8e
textencoder <160430049+textencoder@users.noreply.github.com>
2024-10-24 21:10:24 -0500

    hex-gen 2.0
    - updated nav bar/buttons
    - updated hex-gen logo

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-const body = document.querySelector('body');
-const hexContainer = document.getElementById('hex-container');
-const paletteContainer = document.getElementById('palette-container');
+const body = document.querySelector("body");
+const hexContainer = document.getElementById("hex-container");
+const paletteContainer = document.getElementById("palette-container");
 
-const hexMode = document.getElementById('hex-mode');
-const paletteMode = document.getElementById('palette-mode');
-const copyBtn = document.getElementById('copy');
+const hexMode = document.getElementById("hex-mode");
+const paletteMode = document.getElementById("palette-mode");
+const copyBtn = document.getElementById("copy");
 
 hexMode.addEventListener("click", () => {
-    paletteContainer.style.display = "none"
-    hexContainer.style.display = "flex";
-    returnHexCode();
-})
+  paletteContainer.style.display = "none";
+  hexContainer.style.display = "flex";
+  returnHexCode();
+});
 
 paletteMode.addEventListener("click", () => {
-    hexContainer.style.display = "none"
-    paletteContainer.style.display = "flex";
-    returnPalette();
-})
+  hexContainer.style.display = "none";
+  paletteContainer.style.display = "flex";
+  returnPalette();
+});
 
-const characters = 'ABCDEF0123456789';
+const characters = "ABCDEF0123456789";
 
 const randomHexCharacter = () => {
-    let randomNumber = Math.floor(Math.random() * 16);
-    return characters.charAt(randomNumber)
-}
+  let randomNumber = Math.floor(Math.random() * 16);
+  return characters.charAt(randomNumber);
+};
 
 const returnHexCode = () => {
-    let hexString = '#';
-    for (let i = 0; i < 6; i++) {
+  let hexString = "#";
+  for (let i = 0; i < 6; i++) {
     hexString += randomHexCharacter();
-    }
-    hexContainer.style.backgroundColor = hexString;
-    hexContainer.innerText = hexString;
-    return hexString;
-}   
+  }
+  hexContainer.style.backgroundColor = hexString;
+  hexContainer.innerText = hexString;
+  return hexString;
+};
 
 const returnPalette = () => {
-    let paletteArray = [];
-    for (let i = 0; i < 5; i++) {
-        paletteArray.push(returnHexCode());
-    }
-    paletteContainer.innerHTML = ``;
-    paletteArray.forEach(hex => {
-        paletteContainer.innerHTML += `<div class="palette" style="background: ${hex}">${hex}</div>`;
-    })
-}
+  let paletteArray = [];
+  for (let i = 0; i < 5; i++) {
+    paletteArray.push(returnHexCode());
+  }
+  paletteContainer.innerHTML = ``;
+  paletteArray.forEach((hex) => {
+    paletteContainer.innerHTML += `<div class="palette" style="background: ${hex}">${hex}</div>`;
+  });
+};
 
 //window.addEventListener("load", returnHexCode, false);
 hexContainer.addEventListener("click", returnHexCode);
 window.addEventListener("load", returnPalette, false);
-paletteContainer.addEventListener('click', returnPalette);
+paletteContainer.addEventListener("click", returnPalette);
 
 //copy to clipboard
-copyBtn.addEventListener('click', () => {
-    if (hexContainer.style.display == 'none') {
-        navigator.clipboard.writeText(paletteContainer.innerText);
-    } else {
-        navigator.clipboard.writeText(hexContainer.innerText);
-    }  
-})
+copyBtn.addEventListener("click", () => {
+  if (hexContainer.style.display == "none") {
+    navigator.clipboard.writeText(paletteContainer.innerText);
+  } else {
+    navigator.clipboard.writeText(hexContainer.innerText);
+  }
+});
